34cf29c8f8c3a5d80969a9cd1b0697cb94c1479e
martins20 <pdesignerpb@gmail.com>
2020-10-23 23:17:14 -0300

    Setting: Creates user entitie on database

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,55 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    second_name: string;
+
+    @Column()
+    cpf: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    cep: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    number: number;
+
+    @Column()
+    complement: string;
+
+    @Column()
+    state: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    password_hash: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
---
+++ src/shared/infra/typeorm/migrations/1603501468165-CreateUsers.ts
@@ -0,0 +1,98 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1603501468165 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        isUnique: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'first_name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'second_name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'cpf',
+                        type: 'varchar',
+                        isNullable: false,
+                        length: '11',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true,
+                    },
+                    {
+                        name: 'phone',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'cep',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'address',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'number',
+                        type: 'integer',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'complement',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'state',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'city',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'password_hash',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+}
