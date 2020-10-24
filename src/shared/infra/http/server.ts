d8f4063e483cc9dfa078cdc26986d7ec286a4beb
martins20 <pdesignerpb@gmail.com>
2020-10-23 23:41:35 -0300

    Feature: Handling Errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,33 @@
-import express from 'express'
-import cors from 'cors'
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 
-import routes from '@shared/infra/http/routes'
+import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
-app.use(express.json())
-app.use(cors())
-app.use(routes)
+// Handling Errors
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+    });
+});
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!')
-})
+    console.log('Server started on port 3333!');
+});
