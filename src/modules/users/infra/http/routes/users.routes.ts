84e0678fb7d1959d6a6df0a50db07d188b4477a0
martins20 <pdesignerpb@gmail.com>
2020-10-24 00:13:39 -0300

    Feature: user can register an account

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import UserController from '../controllers/UsersController';
+
 const UsersRouter = Router();
 
-UsersRouter.post('/', () => {});
+const userController = new UserController();
+
+UsersRouter.post('/', userController.create);
 
 export default UsersRouter;
