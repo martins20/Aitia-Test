29a9a652dff2a011dcb8b68630d466328a34e279
martins20 <pdesignerpb@gmail.com>
2020-10-23 23:55:57 -0300

    Setting & Fix: Create user repository

---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,8 @@
+import User from '../infra/typeorm/entities/User';
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
+
+export default interface IUserRepository {
+    findByEmail(email: string): Promise<User | undefined>;
+    create(data: ICreateUserDTO): Promise<User>;
+    save(user: User): Promise<User>;
+}
